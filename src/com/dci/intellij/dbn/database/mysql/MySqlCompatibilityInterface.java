584162e83c3bdc31eee6eab0c44ab9ab2589e219
dancioca <dancioca@bluewin.ch>
2014-08-06 14:59:10 +0200

    [NEW] MySql column sorting (support for nulls first)

--- src/com/dci/intellij/dbn/database/mysql/MySqlCompatibilityInterface.java
+++ src/com/dci/intellij/dbn/database/mysql/MySqlCompatibilityInterface.java
@@ -1,5 +1,6 @@
 package com.dci.intellij.dbn.database.mysql;
 
+import com.dci.intellij.dbn.data.sorting.SortDirection;
 import com.dci.intellij.dbn.database.DatabaseCompatibilityInterface;
 import com.dci.intellij.dbn.database.DatabaseFeature;
 import com.dci.intellij.dbn.database.DatabaseObjectTypeId;
@@ -49,5 +50,11 @@ public class MySqlCompatibilityInterface extends DatabaseCompatibilityInterface 
 
     public char getIdentifierQuotes() {
         return '`';
+    }
+
+    @Override
+    public String getOrderByClause(String columnName, SortDirection sortDirection, boolean nullsFirst) {
+        nullsFirst = (nullsFirst && sortDirection == SortDirection.ASCENDING) || (!nullsFirst && sortDirection == SortDirection.DESCENDING);
+        return "(" + columnName + " is" + (nullsFirst ? "" : " not") + " null), " + columnName + " " + sortDirection.getSqlToken();
     }
 }

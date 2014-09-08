c3394b5613a1dd5ee5250593b21cf859fb3ffc06
dancioca <dancioca@bluewin.ch>
2014-09-08 17:27:08 +0200

    [FIX]  layout issues (missing borders to scrollable contents)

--- src/com/dci/intellij/dbn/connection/config/ui/ConnectionFilterSettingsForm.java
+++ src/com/dci/intellij/dbn/connection/config/ui/ConnectionFilterSettingsForm.java
@@ -1,15 +1,19 @@
 package com.dci.intellij.dbn.connection.config.ui;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
 import com.dci.intellij.dbn.common.options.ui.CompositeConfigurationEditorForm;
 import com.dci.intellij.dbn.connection.config.ConnectionFilterSettings;
+import com.intellij.openapi.options.ConfigurationException;
 
 public class ConnectionFilterSettingsForm extends CompositeConfigurationEditorForm<ConnectionFilterSettings>{
     private JPanel mainPanel;
     private JPanel objectTypesFilterPanel;
     private JPanel objectNameFiltersPanel;
+    private JCheckBox hideEmptySchemasCheckBox;
 
     public ConnectionFilterSettingsForm(ConnectionFilterSettings settings) {
         super(settings);
@@ -19,6 +23,16 @@ public class ConnectionFilterSettingsForm extends CompositeConfigurationEditorFo
 
     public JComponent getComponent() {
         return mainPanel;
+    }
+
+    @Override
+    public void applyChanges() throws ConfigurationException {
+
+    }
+
+    @Override
+    public void resetChanges() {
+        super.resetChanges();
     }
 
     @Override

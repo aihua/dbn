cbb98fbf19d9af41885560c4bfd64fe7c92ae29b
dancioca <dancioca@bluewin.ch>
2014-11-21 14:31:43 +0100

    [FIX] random user reported issues

--- src/com/dci/intellij/dbn/code/common/intention/ExecuteStatementIntentionAction.java
+++ src/com/dci/intellij/dbn/code/common/intention/ExecuteStatementIntentionAction.java
@@ -15,6 +15,7 @@ import com.dci.intellij.dbn.language.common.psi.PsiUtil;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.IncorrectOperationException;
 
@@ -35,15 +36,18 @@ public class ExecuteStatementIntentionAction extends GenericIntentionAction {
     }
 
     public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile psiFile) {
-        if (psiFile.getVirtualFile().getFileType() instanceof DBLanguageFileType) {
-            ExecutablePsiElement executable = PsiUtil.lookupExecutableAtCaret(editor, true);
-            FileEditor fileEditor = EditorUtil.getFileEditor(editor);
-            if (executable != null && fileEditor != null) {
-                StatementExecutionManager executionManager = StatementExecutionManager.getInstance(project);
-                StatementExecutionProcessor executionProcessor = executionManager.getExecutionProcessor(fileEditor, executable, true);
-                if (executionProcessor != null) {
-                    StatementExecutionResult executionResult = executionProcessor.getExecutionResult();
-                    return executionResult != null;
+        if (psiFile != null) {
+            VirtualFile virtualFile = psiFile.getVirtualFile();
+            if (virtualFile != null && virtualFile.getFileType() instanceof DBLanguageFileType) {
+                ExecutablePsiElement executable = PsiUtil.lookupExecutableAtCaret(editor, true);
+                FileEditor fileEditor = EditorUtil.getFileEditor(editor);
+                if (executable != null && fileEditor != null) {
+                    StatementExecutionManager executionManager = StatementExecutionManager.getInstance(project);
+                    StatementExecutionProcessor executionProcessor = executionManager.getExecutionProcessor(fileEditor, executable, true);
+                    if (executionProcessor != null) {
+                        StatementExecutionResult executionResult = executionProcessor.getExecutionResult();
+                        return executionResult != null;
+                    }
                 }
             }
         }

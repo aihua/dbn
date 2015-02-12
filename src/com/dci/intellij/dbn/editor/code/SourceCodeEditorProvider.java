cdd22747fbe246e97e9ef0816b688375be9edbb0
Dan Cioca <dancioca@bluewin.ch>
2015-02-12 22:30:17 +0100

    [FIX] NPE file editor provider

--- src/com/dci/intellij/dbn/editor/code/SourceCodeEditorProvider.java
+++ src/com/dci/intellij/dbn/editor/code/SourceCodeEditorProvider.java
@@ -2,6 +2,7 @@ package com.dci.intellij.dbn.editor.code;
 
 import com.dci.intellij.dbn.editor.DBContentType;
 import com.dci.intellij.dbn.editor.EditorProviderId;
+import com.dci.intellij.dbn.object.common.DBSchemaObject;
 import com.dci.intellij.dbn.vfs.DBEditableObjectVirtualFile;
 import com.intellij.openapi.fileEditor.FileEditorPolicy;
 import com.intellij.openapi.project.Project;
@@ -16,8 +17,11 @@ public class SourceCodeEditorProvider extends BasicSourceCodeEditorProvider {
     public boolean accept(@NotNull Project project, @NotNull VirtualFile virtualFile) {
         if (virtualFile instanceof DBEditableObjectVirtualFile) {
             DBEditableObjectVirtualFile databaseFile = (DBEditableObjectVirtualFile) virtualFile;
-            DBContentType contentType = databaseFile.getObject().getContentType();
-            return contentType.isOneOf(DBContentType.CODE, DBContentType.CODE_AND_DATA);
+            DBSchemaObject object = databaseFile.getObject();
+            if (object != null) {
+                DBContentType contentType = object.getContentType();
+                return contentType.isOneOf(DBContentType.CODE, DBContentType.CODE_AND_DATA);
+            }
 
         }
         return false;

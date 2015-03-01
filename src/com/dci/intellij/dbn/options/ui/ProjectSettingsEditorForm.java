18ec670b223255e30d6d66cde8622a3c1be00bf5
Dan Cioca <dancioca@bluewin.ch>
2015-03-01 22:41:53 +0100

    [NEW] plugin update direct link

--- src/com/dci/intellij/dbn/options/ui/ProjectSettingsEditorForm.java
+++ src/com/dci/intellij/dbn/options/ui/ProjectSettingsEditorForm.java
@@ -1,21 +1,14 @@
 package com.dci.intellij.dbn.options.ui;
 
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.event.HyperlinkEvent;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import com.dci.intellij.dbn.DatabaseNavigator;
 import com.dci.intellij.dbn.browser.options.DatabaseBrowserSettings;
 import com.dci.intellij.dbn.code.common.completion.options.CodeCompletionSettings;
 import com.dci.intellij.dbn.common.Icons;
+import com.dci.intellij.dbn.common.notification.NotificationUtil;
 import com.dci.intellij.dbn.common.options.Configuration;
 import com.dci.intellij.dbn.common.options.ui.CompositeConfigurationEditorForm;
 import com.dci.intellij.dbn.common.options.ui.ConfigurationEditorForm;
+import com.dci.intellij.dbn.common.thread.BackgroundTask;
 import com.dci.intellij.dbn.common.ui.tab.TabbedPane;
 import com.dci.intellij.dbn.common.util.StringUtil;
 import com.dci.intellij.dbn.connection.ConnectionHandler;
@@ -31,10 +24,15 @@ import com.dci.intellij.dbn.navigation.options.NavigationSettings;
 import com.dci.intellij.dbn.options.ConfigId;
 import com.dci.intellij.dbn.options.ProjectSettings;
 import com.dci.intellij.dbn.options.general.GeneralProjectSettings;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginInstaller;
+import com.intellij.ide.plugins.PluginManagerMain;
+import com.intellij.ide.plugins.PluginNode;
+import com.intellij.ide.plugins.RepositoryHelper;
 import com.intellij.openapi.editor.colors.EditorColors;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.openapi.updateSettings.impl.UpdateChecker;
-import com.intellij.openapi.updateSettings.impl.UpdateSettings;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.ui.HyperlinkAdapter;
 import com.intellij.ui.HyperlinkLabel;
@@ -42,6 +40,17 @@ import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.tabs.TabInfo;
 import com.intellij.util.ui.PlatformColors;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.event.HyperlinkEvent;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProjectSettingsEditorForm extends CompositeConfigurationEditorForm<ProjectSettings> {
     private JPanel mainPanel;
@@ -99,7 +108,39 @@ public class ProjectSettingsEditorForm extends CompositeConfigurationEditorForm<
                 @Override
                 protected void hyperlinkActivated(HyperlinkEvent e) {
                     if (dialog != null) dialog.doCancelAction();
-                    UpdateChecker.updateAndShowResult(generalSettings.getProject(), UpdateSettings.getInstance());
+
+                    final Project project = generalSettings.getProject();
+                    new BackgroundTask(project, "Updating Plugin", false) {
+                        @Override
+                        protected void execute(@NotNull ProgressIndicator progressIndicator) throws InterruptedException {
+                            try {
+                                List<PluginNode> updateDescriptors = new ArrayList<PluginNode>();
+                                List<IdeaPluginDescriptor> descriptors = RepositoryHelper.loadCachedPlugins();
+                                if (descriptors != null) {
+                                    for (IdeaPluginDescriptor descriptor : descriptors) {
+                                        if (descriptor.getPluginId().toString().equals(DatabaseNavigator.DBN_PLUGIN_ID)) {
+                                            PluginNode pluginNode = new PluginNode(descriptor.getPluginId());
+                                            pluginNode.setName(descriptor.getName());
+                                            pluginNode.setSize("-1");
+                                            pluginNode.setRepositoryName(PluginInstaller.UNKNOWN_HOST_MARKER);
+                                            updateDescriptors.add(pluginNode);
+                                            break;
+                                        }
+                                    }
+                                }
+
+                                PluginManagerMain.downloadPlugins(updateDescriptors, descriptors, new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        PluginManagerMain.notifyPluginsUpdated(project);
+                                    }
+                                }, null);
+                                //UpdateChecker.updateAndShowResult(generalSettings.getProject(), UpdateSettings.getInstance());
+                            } catch (Exception ex) {
+                                NotificationUtil.sendErrorNotification(project, "Update Error", "Error updating DBN plugin: " + ex.getMessage());
+                            }
+                        }
+                    }.start();
                 }
             });
             pluginUpdateLinkPanel.add(label, BorderLayout.WEST);

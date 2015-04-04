7730a256b9cdf7ab9f0da71c98197dcc207ef0e7
Dan Cioca <dancioca@bluewin.ch>
2015-04-04 13:57:52 +0200

    [FIX] cyclic component initialization

--- src/com/dci/intellij/dbn/browser/DatabaseBrowserManager.java
+++ src/com/dci/intellij/dbn/browser/DatabaseBrowserManager.java
@@ -1,13 +1,5 @@
 package com.dci.intellij.dbn.browser;
 
-import javax.swing.tree.TreePath;
-import java.util.ArrayList;
-import java.util.List;
-import org.jdom.Element;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import com.dci.intellij.dbn.browser.model.BrowserTreeModel;
 import com.dci.intellij.dbn.browser.model.BrowserTreeNode;
 import com.dci.intellij.dbn.browser.model.TabbedBrowserTreeModel;
@@ -48,6 +40,14 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
+import org.jdom.Element;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.tree.TreePath;
+import java.util.ArrayList;
+import java.util.List;
 
 @State(
     name = "DBNavigator.Project.DatabaseBrowserManager",
@@ -214,14 +214,18 @@ public class DatabaseBrowserManager extends AbstractProjectComponent implements 
     private ConnectionManagerListener connectionManagerListener = new ConnectionManagerListener() {
         @Override
         public void connectionsChanged() {
-            getToolWindowForm().getBrowserForm().rebuild();
+            if (toolWindowForm.isLoaded()) {
+                getToolWindowForm().getBrowserForm().rebuild();
+            }
         }
     };
 
     private ObjectFilterChangeListener filterChangeListener = new ObjectFilterChangeListener() {
         public void typeFiltersChanged(ConnectionHandler connectionHandler) {
             if (connectionHandler == null) {
-                getToolWindowForm().getBrowserForm().rebuildTree();
+                if (toolWindowForm.isLoaded()) {
+                    getToolWindowForm().getBrowserForm().rebuildTree();
+                }
             } else {
                 connectionHandler.getObjectBundle().rebuildTreeChildren();
             }

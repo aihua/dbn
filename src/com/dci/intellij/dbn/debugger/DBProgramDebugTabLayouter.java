a1384d363bc925cdb55caf4dc3e85fafb141e102
Dan Cioca <dancioca@bluewin.ch>
2015-04-12 16:02:17 +0200

    [FIX] debugger console layout

--- src/com/dci/intellij/dbn/debugger/DBProgramDebugTabLayouter.java
+++ src/com/dci/intellij/dbn/debugger/DBProgramDebugTabLayouter.java
@@ -4,10 +4,7 @@ import com.intellij.debugger.ui.DebuggerContentInfo;
 import com.intellij.execution.ui.ExecutionConsole;
 import com.intellij.execution.ui.RunnerLayoutUi;
 import com.intellij.execution.ui.layout.LayoutViewOptions;
-import com.intellij.execution.ui.layout.PlaceInGrid;
-import com.intellij.icons.AllIcons;
 import com.intellij.ui.content.Content;
-import com.intellij.xdebugger.XDebuggerBundle;
 import com.intellij.xdebugger.ui.XDebugTabLayouter;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,13 +12,18 @@ public class DBProgramDebugTabLayouter extends XDebugTabLayouter {
     @NotNull
     @Override
     public Content registerConsoleContent(@NotNull RunnerLayoutUi ui, @NotNull ExecutionConsole console) {
+        Content consoleContent = super.registerConsoleContent(ui, console);
+        ui.getDefaults().initFocusContent(DebuggerContentInfo.FRAME_CONTENT, LayoutViewOptions.STARTUP);
+        return consoleContent;
+/*
         Content content = ui.createContent(DebuggerContentInfo.CONSOLE_CONTENT, console.getComponent(),
                 XDebuggerBundle.message("debugger.session.tab.console.content.name"),
                 AllIcons.Debugger.Console,
                 console.getPreferredFocusableComponent());
         content.setCloseable(false);
-        ui.addContent(content, 0, PlaceInGrid.bottom, false);
+        ui.addContent(content, 1, PlaceInGrid.center, false);
         ui.getDefaults().initFocusContent(DebuggerContentInfo.FRAME_CONTENT, LayoutViewOptions.STARTUP);
         return content;
+*/
     }
 }

578c761d9bda0926fc3a6d4169d4f43703b2ff10
dancioca <dancioca@bluewin.ch>
2015-05-04 12:06:04 +0200

    [NEW] SQL structure (top level elements filter)

--- src/com/dci/intellij/dbn/language/sql/structure/SQLStructureViewModel.java
+++ src/com/dci/intellij/dbn/language/sql/structure/SQLStructureViewModel.java
@@ -4,10 +4,12 @@ import org.jetbrains.annotations.NotNull;
 
 import com.dci.intellij.dbn.language.common.structure.DBLanguageStructureViewModel;
 import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.ide.util.treeView.smartTree.Filter;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
 
 public class SQLStructureViewModel extends DBLanguageStructureViewModel {
+    private Filter[] filters = new Filter[]{new SQLStructureViewModelFilter()};
 
     public SQLStructureViewModel(Editor editor, PsiFile psiFile) {
         super(editor, psiFile);
@@ -16,5 +18,11 @@ public class SQLStructureViewModel extends DBLanguageStructureViewModel {
     @NotNull
     public StructureViewTreeElement getRoot() {
         return new SQLStructureViewElement(getPsiFile());
+    }
+
+    @NotNull
+    @Override
+    public Filter[] getFilters() {
+        return filters;
     }
 }
---
+++ src/com/dci/intellij/dbn/language/sql/structure/SQLStructureViewModelFilter.java
@@ -0,0 +1,69 @@
+package com.dci.intellij.dbn.language.sql.structure;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.dci.intellij.dbn.common.Icons;
+import com.dci.intellij.dbn.language.common.element.util.ElementTypeAttribute;
+import com.dci.intellij.dbn.language.common.psi.BasePsiElement;
+import com.dci.intellij.dbn.language.common.psi.ChameleonPsiElement;
+import com.dci.intellij.dbn.language.common.psi.IdentifierPsiElement;
+import com.dci.intellij.dbn.language.common.psi.NamedPsiElement;
+import com.dci.intellij.dbn.language.psql.PSQLLanguage;
+import com.dci.intellij.dbn.object.common.DBObjectType;
+import com.intellij.ide.util.treeView.smartTree.ActionPresentation;
+import com.intellij.ide.util.treeView.smartTree.ActionPresentationData;
+import com.intellij.ide.util.treeView.smartTree.Filter;
+import com.intellij.ide.util.treeView.smartTree.TreeElement;
+import com.intellij.psi.PsiElement;
+
+public class SQLStructureViewModelFilter implements Filter {
+    private ActionPresentation actionPresentation = new ActionPresentationData("Top Level Elements", "", Icons.TOP_LEVEL_FILTER);
+
+    @NotNull
+    public ActionPresentation getPresentation() {
+        return actionPresentation;
+    }
+
+    @NotNull
+    public String getName() {
+        return "Top Level";
+    }
+
+    @Override
+    public boolean isVisible(TreeElement treeNode) {
+        if (treeNode instanceof SQLStructureViewElement) {
+            SQLStructureViewElement structureViewElement = (SQLStructureViewElement) treeNode;
+            PsiElement psiElement = structureViewElement.getPsiElement();
+            if (psiElement.getLanguage()== PSQLLanguage.INSTANCE) {
+                if (psiElement instanceof ChameleonPsiElement) {
+                    return true;
+                }
+                if (psiElement instanceof NamedPsiElement) {
+                    NamedPsiElement namedPsiElement = (NamedPsiElement) psiElement;
+                    boolean isObject = namedPsiElement.is(ElementTypeAttribute.OBJECT_DEFINITION) ||
+                            namedPsiElement.is(ElementTypeAttribute.OBJECT_DECLARATION) ||
+                            namedPsiElement.is(ElementTypeAttribute.OBJECT_SPECIFICATION);
+
+                    if (isObject) {
+                        BasePsiElement subject = namedPsiElement.findFirstPsiElement(ElementTypeAttribute.SUBJECT);
+                        if (subject instanceof IdentifierPsiElement) {
+                            IdentifierPsiElement identifierPsiElement = (IdentifierPsiElement) subject;
+                            DBObjectType objectType = identifierPsiElement.getObjectType();
+                            if (objectType.matches(DBObjectType.METHOD) || objectType.matches(DBObjectType.PROGRAM)) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean isReverted() {
+        return false;
+    }
+}

de9b82f36754b02d17f3da65e8c9a01c899bbaa9
Dan Cioca <dancioca@bluewin.ch>
2015-05-10 01:02:55 +0200

    [NEW] script execution (postgres support)

--- src/com/dci/intellij/dbn/database/mysql/MySqlExecutionInterface.java
+++ src/com/dci/intellij/dbn/database/mysql/MySqlExecutionInterface.java
@@ -1,9 +1,5 @@
 package com.dci.intellij.dbn.database.mysql;
 
-import java.util.List;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import com.dci.intellij.dbn.common.database.AuthenticationInfo;
 import com.dci.intellij.dbn.common.database.DatabaseInfo;
 import com.dci.intellij.dbn.common.util.StringUtil;
@@ -12,6 +8,10 @@ import com.dci.intellij.dbn.database.common.DatabaseExecutionInterfaceImpl;
 import com.dci.intellij.dbn.database.common.execution.MethodExecutionProcessor;
 import com.dci.intellij.dbn.execution.script.CmdLineInterface;
 import com.dci.intellij.dbn.object.DBMethod;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MySqlExecutionInterface extends DatabaseExecutionInterfaceImpl {
 
@@ -34,16 +34,21 @@ public class MySqlExecutionInterface extends DatabaseExecutionInterfaceImpl {
         command.add("--host=" + databaseInfo.getHost());
 
         String port = databaseInfo.getPort();
-        if (StringUtil.isNotEmpty(port)) command.add("--port=" + port);
+        if (StringUtil.isNotEmpty(port)) {
+            command.add("--port=" + port);
+        }
+
+        String database = databaseInfo.getDatabase();
+        if (StringUtil.isNotEmpty(database)) {
+            command.add("--database=" + database);
+        }
+
 
         command.add("--user=" + authenticationInfo.getUser());
         command.add("--password=" + authenticationInfo.getPassword());
 
         command.add("-e");
         command.add("\"source " + filePath + "\"");
-
-        String database = databaseInfo.getDatabase();
-        if (StringUtil.isNotEmpty(database)) command.add(database);
 
         //command.add("< " + filePath);
 
--- src/com/dci/intellij/dbn/database/postgres/PostgresExecutionInterface.java
+++ src/com/dci/intellij/dbn/database/postgres/PostgresExecutionInterface.java
@@ -1,16 +1,18 @@
 package com.dci.intellij.dbn.database.postgres;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import com.dci.intellij.dbn.common.database.AuthenticationInfo;
 import com.dci.intellij.dbn.common.database.DatabaseInfo;
+import com.dci.intellij.dbn.common.util.StringUtil;
 import com.dci.intellij.dbn.database.ScriptExecutionInput;
 import com.dci.intellij.dbn.database.common.DatabaseExecutionInterfaceImpl;
 import com.dci.intellij.dbn.database.common.execution.MethodExecutionProcessor;
 import com.dci.intellij.dbn.database.postgres.execution.PostgresMethodExecutionProcessor;
 import com.dci.intellij.dbn.execution.script.CmdLineInterface;
 import com.dci.intellij.dbn.object.DBMethod;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PostgresExecutionInterface extends DatabaseExecutionInterfaceImpl {
     public MethodExecutionProcessor createExecutionProcessor(DBMethod method) {
@@ -23,6 +25,42 @@ public class PostgresExecutionInterface extends DatabaseExecutionInterfaceImpl {
 
     @Override
     public ScriptExecutionInput createScriptExecutionInput(@NotNull CmdLineInterface cmdLineInterface, @NotNull String filePath, String content, @Nullable String schema, @NotNull DatabaseInfo databaseInfo, @NotNull AuthenticationInfo authenticationInfo) {
-        return null;
+        ScriptExecutionInput executionInput = new ScriptExecutionInput(content);
+
+        List<String> command = executionInput.getCommand();
+        command.add(cmdLineInterface.getExecutablePath());
+        command.add("--echo-all");
+        command.add("--host=" + databaseInfo.getHost());
+
+        String port = databaseInfo.getPort();
+        if (StringUtil.isNotEmpty(port)) {
+            command.add("--port=" + port);
+        }
+
+        String database = databaseInfo.getDatabase();
+        if (StringUtil.isNotEmpty(database)) {
+            command.add("--dbname=" + database);
+        }
+
+        command.add("--username=" + authenticationInfo.getUser());
+        if (authenticationInfo.isEmptyPassword()) {
+            command.add("--no-password");
+        } else {
+            executionInput.getEnvironmentVars().put("PGPASSWORD", authenticationInfo.getPassword());
+        }
+
+
+        command.add("-f");
+        command.add("\"" + filePath + "\"");
+
+
+        //command.add("< " + filePath);
+
+        StringBuilder contentBuilder = executionInput.getContent();
+        if (StringUtil.isNotEmpty(schema)) {
+            contentBuilder.insert(0, "set search_path to " + schema + ";\n");
+        }
+        //contentBuilder.append("\nexit;\n");
+        return executionInput;
     }
 }

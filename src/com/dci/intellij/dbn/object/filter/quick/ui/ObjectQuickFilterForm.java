e0c2d0efaa5469ab13638576ded00446e9846c61
Dan Cioca <dancioca@bluewin.ch>
2015-06-21 22:40:57 +0200

    [NEW] filter object lists (filter dialog cosmetics)

--- src/com/dci/intellij/dbn/object/filter/quick/ui/ObjectQuickFilterForm.java
+++ src/com/dci/intellij/dbn/object/filter/quick/ui/ObjectQuickFilterForm.java
@@ -1,17 +1,8 @@
 package com.dci.intellij.dbn.object.filter.quick.ui;
 
-import javax.swing.BoxLayout;
-import javax.swing.Icon;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import org.jetbrains.annotations.NotNull;
-
 import com.dci.intellij.dbn.common.Icons;
+import com.dci.intellij.dbn.common.ui.Borders;
+import com.dci.intellij.dbn.common.ui.DBNComboBox;
 import com.dci.intellij.dbn.common.ui.DBNFormImpl;
 import com.dci.intellij.dbn.common.ui.DBNHeaderForm;
 import com.dci.intellij.dbn.common.ui.ValueSelector;
@@ -22,18 +13,32 @@ import com.dci.intellij.dbn.connection.ConnectionHandler;
 import com.dci.intellij.dbn.connection.GenericDatabaseElement;
 import com.dci.intellij.dbn.object.DBSchema;
 import com.dci.intellij.dbn.object.common.list.DBObjectList;
+import com.dci.intellij.dbn.object.filter.ConditionJoinType;
 import com.dci.intellij.dbn.object.filter.ConditionOperator;
 import com.dci.intellij.dbn.object.filter.quick.ObjectQuickFilter;
 import com.dci.intellij.dbn.object.filter.quick.ObjectQuickFilterCondition;
 import com.dci.intellij.dbn.object.filter.quick.ObjectQuickFilterManager;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.util.PlatformIcons;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ObjectQuickFilterForm extends DBNFormImpl<ObjectQuickFilterDialog> {
     private JPanel mainPanel;
     private JPanel headerPanel;
     private JPanel conditionsPanel;
     private JPanel actionsPanel;
+    private DBNComboBox<ConditionJoinType> joinTypeComboBox;
+    private JPanel joinTypePanel;
 
     private List<ObjectQuickFilterConditionForm> conditionForms = new ArrayList<ObjectQuickFilterConditionForm>();
     private DBObjectList objectList;
@@ -43,6 +48,7 @@ public class ObjectQuickFilterForm extends DBNFormImpl<ObjectQuickFilterDialog> 
         super(parent);
         this.objectList = objectList;
         conditionsPanel.setLayout(new BoxLayout(conditionsPanel, BoxLayout.Y_AXIS));
+        conditionsPanel.setBorder(Borders.BOTTOM_LINE_BORDER);
         filter = objectList.getQuickFilter();
         if (filter == null) {
             filter = new ObjectQuickFilter(objectList.getObjectType());
@@ -59,7 +65,17 @@ public class ObjectQuickFilterForm extends DBNFormImpl<ObjectQuickFilterDialog> 
             ObjectQuickFilterCondition condition = filter.addNewCondition(quickFilterManager.getLastUsedOperator());
             addConditionPanel(condition);*/
         }
+        joinTypePanel.setVisible(conditions.size() > 1);
         actionsPanel.add(new NewFilterSelector(filter), BorderLayout.CENTER);
+
+        joinTypeComboBox.setValues(ConditionJoinType.values());
+        joinTypeComboBox.setSelectedValue(filter.getJoinType());
+        joinTypeComboBox.addListener(new ValueSelectorListener<ConditionJoinType>() {
+            @Override
+            public void selectionChanged(ConditionJoinType oldValue, ConditionJoinType newValue) {
+                filter.setJoinType(newValue);
+            }
+        });
 
         Icon headerIcon = Icons.DATASET_FILTER;
         ConnectionHandler connectionHandler = objectList.getConnectionHandler();
@@ -77,6 +93,7 @@ public class ObjectQuickFilterForm extends DBNFormImpl<ObjectQuickFilterDialog> 
         conditionsPanel.add(conditionForm.getComponent());
         conditionsPanel.revalidate();
         conditionsPanel.repaint();
+        joinTypePanel.setVisible(filter.getConditions().size() > 1);
 
         conditionForms.add(conditionForm);
         Disposer.register(this, conditionForm);
@@ -94,6 +111,7 @@ public class ObjectQuickFilterForm extends DBNFormImpl<ObjectQuickFilterDialog> 
                 break;
             }
         }
+        joinTypePanel.setVisible(filter.getConditions().size() > 1);
         conditionsPanel.revalidate();
         conditionsPanel.repaint();
     }

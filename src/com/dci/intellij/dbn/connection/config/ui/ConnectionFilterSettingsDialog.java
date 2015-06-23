2103f821bdf40f8f077131eb4247ca6195ee21f2
dancioca <dancioca@bluewin.ch>
2015-06-23 16:40:48 +0200

    [FIX] connection filter dialog (cosmetics)

--- src/com/dci/intellij/dbn/connection/config/ui/ConnectionFilterSettingsDialog.java
+++ src/com/dci/intellij/dbn/connection/config/ui/ConnectionFilterSettingsDialog.java
@@ -4,20 +4,40 @@ import javax.swing.Action;
 import javax.swing.JComponent;
 import org.jetbrains.annotations.NotNull;
 
+import com.dci.intellij.dbn.common.ui.DBNContentWithHeaderForm;
+import com.dci.intellij.dbn.common.ui.DBNForm;
+import com.dci.intellij.dbn.common.ui.DBNHeaderForm;
 import com.dci.intellij.dbn.common.ui.dialog.DBNDialog;
 import com.dci.intellij.dbn.common.util.MessageUtil;
 import com.dci.intellij.dbn.connection.ConnectionHandler;
 import com.dci.intellij.dbn.connection.config.ConnectionFilterSettings;
+import com.dci.intellij.dbn.connection.config.ConnectionSettings;
+import com.dci.intellij.dbn.options.ProjectSettingsManager;
 import com.intellij.openapi.options.ConfigurationException;
 
-public class ConnectionFilterSettingsDialog extends DBNDialog<ConnectionFilterSettingsDialogForm> {
-    public ConnectionFilterSettingsDialog(@NotNull ConnectionHandler connectionHandler) {
+public class ConnectionFilterSettingsDialog extends DBNDialog<DBNContentWithHeaderForm<ConnectionFilterSettingsDialog>> {
+    private ConnectionFilterSettingsForm configurationEditor;
+    public ConnectionFilterSettingsDialog(@NotNull final ConnectionHandler connectionHandler) {
         super(connectionHandler.getProject(), "Object Filters", true);
-        component = new ConnectionFilterSettingsDialogForm(this, connectionHandler);
+        component = new DBNContentWithHeaderForm<ConnectionFilterSettingsDialog>(this) {
+            @Override
+            public DBNHeaderForm createHeaderForm() {
+                return new DBNHeaderForm(connectionHandler);
+            }
+
+            @Override
+            public DBNForm createContentForm() {
+                ProjectSettingsManager settingsManager = ProjectSettingsManager.getInstance(getProject());
+                ConnectionSettings connectionSettings = settingsManager.getConnectionSettings().getConnectionSettings(connectionHandler.getId());
+                configurationEditor = connectionSettings.getFilterSettings().createConfigurationEditor();
+                return configurationEditor;
+            }
+        };
         setModal(true);
         setResizable(true);
         init();
     }
+
 
     @Override
     public JComponent getPreferredFocusedComponent() {
@@ -35,7 +55,6 @@ public class ConnectionFilterSettingsDialog extends DBNDialog<ConnectionFilterSe
 
     public void doOKAction() {
         try {
-            ConnectionFilterSettingsForm configurationEditor = component.getConfigurationEditor();
             ConnectionFilterSettings configuration = configurationEditor.getConfiguration();
             // !!workaround!! apply settings is normally cascaded from top level settings
             configurationEditor.applyFormChanges();

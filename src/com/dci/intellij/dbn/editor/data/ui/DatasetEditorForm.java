965384e299526742b8f793c9d580f9458cd2a5f4
dancioca@bluewin.ch <dancioca@bluewin.ch>
2015-11-29 20:13:42 +0100

    [FIX] compatibility issues

--- src/com/dci/intellij/dbn/editor/data/ui/DatasetEditorForm.java
+++ src/com/dci/intellij/dbn/editor/data/ui/DatasetEditorForm.java
@@ -1,17 +1,5 @@
 package com.dci.intellij.dbn.editor.data.ui;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableColumn;
-import java.awt.BorderLayout;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import org.jetbrains.annotations.NotNull;
-
 import com.dci.intellij.dbn.common.Icons;
 import com.dci.intellij.dbn.common.dispose.FailsafeUtil;
 import com.dci.intellij.dbn.common.thread.ConditionalLaterInvocator;
@@ -40,6 +28,18 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.util.ui.AsyncProcessIcon;
 import com.intellij.util.ui.UIUtil;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableColumn;
+import java.awt.BorderLayout;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatasetEditorForm extends DBNFormImpl implements SearchableDataComponent {
     private JPanel actionsPanel;
@@ -261,12 +261,12 @@ public class DatasetEditorForm extends DBNFormImpl implements SearchableDataComp
 
         @Override
         public void actionPerformed(AnActionEvent e) {
-            getDatasetEditor().getTableModel().cancelDataLoad();
+            getEditorTable().getModel().cancelDataLoad();
         }
 
         @Override
         public void update(AnActionEvent e) {
-            e.getPresentation().setEnabled(!getDatasetEditor().getTableModel().isLoadCancelled());
+            e.getPresentation().setEnabled(!getEditorTable().getModel().isLoadCancelled());
         }
     }
 }

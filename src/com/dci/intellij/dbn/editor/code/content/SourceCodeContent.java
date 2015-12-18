b497b0036cec8d1a7afc8ab787ce275018d20321
dancioca@bluewin.ch <dancioca@bluewin.ch>
2015-12-18 21:11:13 +0100

    [FIX] compatibility issues

--- src/com/dci/intellij/dbn/editor/code/content/SourceCodeContent.java
+++ src/com/dci/intellij/dbn/editor/code/content/SourceCodeContent.java
@@ -1,16 +1,15 @@
 package com.dci.intellij.dbn.editor.code.content;
 
+import com.dci.intellij.dbn.common.util.StringUtil;
+import com.intellij.openapi.diff.impl.ComparisonPolicy;
+import com.intellij.openapi.diff.impl.processing.ByWord;
+import org.jetbrains.annotations.NotNull;
+
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.jetbrains.annotations.NotNull;
 
-import com.dci.intellij.dbn.common.util.StringUtil;
-import com.intellij.diff.comparison.ByWord;
-import com.intellij.diff.comparison.ComparisonPolicy;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
 import static com.dci.intellij.dbn.editor.code.content.GuardedBlockMarker.END_OFFSET_IDENTIFIER;
 import static com.dci.intellij.dbn.editor.code.content.GuardedBlockMarker.START_OFFSET_IDENTIFIER;
 
@@ -49,10 +48,9 @@ public class SourceCodeContent{
     public boolean matches(SourceCodeContent content, boolean soft) {
         if (soft) {
             try {
-                ProgressIndicator progressIndicator = ProgressManager.getInstance().getProgressIndicator();
-                return ByWord.compare(text, content.text, ComparisonPolicy.TRIM_WHITESPACES, progressIndicator).isEmpty();
+                ByWord byWord = new ByWord(ComparisonPolicy.IGNORE_SPACE);
+                return byWord.buildFragments(text.toString(), content.text.toString()).length == 0;
             } catch (Exception ignore) {
-                ignore.printStackTrace();
             }
         }
         return StringUtil.equals(text, content.text);

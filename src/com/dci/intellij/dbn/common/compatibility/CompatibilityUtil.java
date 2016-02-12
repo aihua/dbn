641a691b01e43400e5cefa8f7db6f0a9c3e279da
dancioca@bluewin.ch <dancioca@bluewin.ch>
2016-02-12 21:43:22 +0100

    [FIX] compatibility issues

--- src/com/dci/intellij/dbn/common/compatibility/CompatibilityUtil.java
+++ src/com/dci/intellij/dbn/common/compatibility/CompatibilityUtil.java
@@ -1,12 +1,5 @@
 package com.dci.intellij.dbn.common.compatibility;
 
-import javax.swing.JComponent;
-import javax.swing.JTextField;
-import javax.swing.UIManager;
-import java.awt.Color;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import com.intellij.find.editorHeaderActions.Utils;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.ex.EditorEx;
@@ -16,6 +9,13 @@ import com.intellij.openapi.util.Key;
 import com.intellij.psi.PsiFile;
 import com.intellij.ui.components.JBList;
 import com.intellij.util.ui.UIUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import java.awt.Color;
 
 public class CompatibilityUtil {
     private static final Key<PsiFile> HARD_REF_TO_PSI = Key.create("HARD_REFERENCE_TO_PSI");
@@ -55,6 +55,6 @@ public class CompatibilityUtil {
     }
 
     public static void cachePsi(@NotNull Document document, @Nullable PsiFile file) {
-        PsiDocumentManagerBase.cachePsi(document, null);
+        CompatibilityUtil.cachePsi(document, null);
     }
 }

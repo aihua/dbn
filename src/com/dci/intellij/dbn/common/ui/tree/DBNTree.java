76c3ae1fdb40b1b1e597ba20279f7fb351f75344
dancioca@bluewin.ch <dancioca@bluewin.ch>
2016-02-14 23:21:29 +0100

    [FIX] browser tree dispose issue

--- src/com/dci/intellij/dbn/common/ui/tree/DBNTree.java
+++ src/com/dci/intellij/dbn/common/ui/tree/DBNTree.java
@@ -1,14 +1,18 @@
 package com.dci.intellij.dbn.common.ui.tree;
 
-import javax.swing.tree.TreeModel;
-import javax.swing.tree.TreeNode;
-
 import com.dci.intellij.dbn.common.dispose.Disposable;
 import com.dci.intellij.dbn.common.ui.GUIUtil;
 import com.intellij.ui.treeStructure.Tree;
 import com.intellij.util.ui.UIUtil;
 
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeNode;
+
 public class DBNTree extends Tree implements Disposable {
+
+    public static final DefaultTreeCellRenderer DEFAULT_CELL_RENDERER = new DefaultTreeCellRenderer();
+
     public DBNTree() {
         setTransferHandler(new DBNTreeTransferHandler());
     }
@@ -32,6 +36,7 @@ public class DBNTree extends Tree implements Disposable {
     public void dispose() {
         if (!disposed) {
             disposed = true;
+            setCellRenderer(DEFAULT_CELL_RENDERER);
             setModel(null);
             setSelectionModel(null);
             GUIUtil.removeListeners(this);

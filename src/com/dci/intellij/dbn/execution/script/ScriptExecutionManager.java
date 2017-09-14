6472c2bd23fccaf13694078c2677398f9559876a
dancioca <dancioca@bluewin.ch>
2017-09-14 13:41:57 +0200

    [FIX] DBN-298 script execution - temp file issues

--- src/com/dci/intellij/dbn/execution/script/ScriptExecutionManager.java
+++ src/com/dci/intellij/dbn/execution/script/ScriptExecutionManager.java
@@ -1,5 +1,25 @@
 package com.dci.intellij.dbn.execution.script;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.security.SecureRandom;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import org.jdesktop.swingx.util.OS;
+import org.jdom.Element;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import com.dci.intellij.dbn.common.AbstractProjectComponent;
 import com.dci.intellij.dbn.common.dispose.FailsafeUtil;
 import com.dci.intellij.dbn.common.message.MessageCallback;
@@ -25,7 +45,12 @@ import com.dci.intellij.dbn.execution.script.options.ScriptExecutionSettings;
 import com.dci.intellij.dbn.execution.script.ui.CmdLineInterfaceInputDialog;
 import com.dci.intellij.dbn.execution.script.ui.ScriptExecutionInputDialog;
 import com.dci.intellij.dbn.object.DBSchema;
-import com.intellij.openapi.components.*;
+import com.intellij.openapi.application.PathManagerEx;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
+import com.intellij.openapi.components.StorageScheme;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.progress.ProcessCanceledException;
@@ -35,24 +60,6 @@ import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jdesktop.swingx.util.OS;
-import org.jdom.Element;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.SQLException;
-import java.sql.SQLTimeoutException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 @State(
         name = "DBNavigator.Project.ScriptExecutionManager",
@@ -61,6 +68,7 @@ import java.util.concurrent.atomic.AtomicReference;
                 @Storage(file = StoragePathMacros.PROJECT_FILE)}
 )
 public class ScriptExecutionManager extends AbstractProjectComponent implements PersistentStateComponent<Element>{
+    private static final SecureRandom TMP_FILE_RANDOMIZER = new SecureRandom();
     private final Map<VirtualFile, Process> activeProcesses = new HashMap<VirtualFile, Process>();
     private Map<DatabaseType, String> recentlyUsedInterfaces = new HashMap<DatabaseType, String>();
     private boolean clearOutputOption = true;
@@ -164,6 +172,9 @@ public class ScriptExecutionManager extends AbstractProjectComponent implements 
                     );
 
                     FileUtil.writeToFile(temporaryScriptFile, executionInput.getTextContent());
+                    if (!temporaryScriptFile.isFile()) {
+                        throw new IllegalStateException("Failed to create temporary script file " + temporaryScriptFile + ". Check access rights at location.");
+                    }
 
                     ProcessBuilder processBuilder = new ProcessBuilder(executionInput.getCommand());
                     processBuilder.environment().putAll(executionInput.getEnvironmentVars());
@@ -258,7 +269,7 @@ public class ScriptExecutionManager extends AbstractProjectComponent implements 
             activeProcesses.remove(sourceFile);
             File temporaryScriptFile = tempScriptFile.get();
             if (temporaryScriptFile != null && temporaryScriptFile.exists()) {
-                temporaryScriptFile.delete();
+                FileUtil.delete(temporaryScriptFile);
             }
         }
     }
@@ -326,7 +337,23 @@ public class ScriptExecutionManager extends AbstractProjectComponent implements 
     }
 
     private File createTempScriptFile() throws IOException {
-        return File.createTempFile("DBN", ".sql");
+        File tempFile = File.createTempFile("DBN-", ".sql");
+        if (!tempFile.isFile()) {
+            long n = TMP_FILE_RANDOMIZER.nextLong();
+            n = n == Long.MIN_VALUE ? 0 : Math.abs(n);
+            String tempFileName = "DBN-" + n;
+
+            tempFile = FileUtil.createTempFile(tempFileName, ".sql");
+            if (!tempFile.isFile()) {
+                Path systemDir = PathManagerEx.getAppSystemDir();
+                File systemTempDir = new File(systemDir.toFile(), "tmp");
+                tempFile = new File(systemTempDir, tempFileName);
+                FileUtil.createParentDirs(tempFile);
+                FileUtil.delete(tempFile);
+                FileUtil.createIfDoesntExist(tempFile);
+            }
+        }
+        return tempFile;
     }
 
     /****************************************

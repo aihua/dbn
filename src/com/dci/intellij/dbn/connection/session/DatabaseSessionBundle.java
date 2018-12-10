969f5e48b8fea6b30b6c30e06469652998280bba
dancioca@bluewin.ch <dancioca@bluewin.ch>
2018-12-10 10:39:56 +0100

    [FIX] connection status

--- src/com/dci/intellij/dbn/connection/session/DatabaseSessionBundle.java
+++ src/com/dci/intellij/dbn/connection/session/DatabaseSessionBundle.java
@@ -1,5 +1,11 @@
 package com.dci.intellij.dbn.connection.session;
 
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import com.dci.intellij.dbn.common.dispose.DisposableBase;
 import com.dci.intellij.dbn.common.dispose.DisposerUtil;
 import com.dci.intellij.dbn.connection.ConnectionHandler;
@@ -8,15 +14,6 @@ import com.dci.intellij.dbn.connection.ConnectionType;
 import com.dci.intellij.dbn.connection.SessionId;
 import com.dci.intellij.dbn.database.DatabaseFeature;
 import com.intellij.openapi.Disposable;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class DatabaseSessionBundle extends DisposableBase implements Disposable{
     private ConnectionHandlerRef connectionHandlerRef;
@@ -46,18 +43,14 @@ public class DatabaseSessionBundle extends DisposableBase implements Disposable{
     }
 
     public List<DatabaseSession> getSessions(ConnectionType ... connectionTypes) {
-        if (connectionTypes == null || connectionTypes.length == 0) {
-            return sessions;
-        } else {
-            List<DatabaseSession> sessions = new ArrayList<>();
-            for (DatabaseSession session : this.sessions) {
-                if (session.getConnectionType().isOneOf(connectionTypes)) {
-                    sessions.add(session);
-                }
+        List<DatabaseSession> sessions = new ArrayList<>();
+        for (DatabaseSession session : this.sessions) {
+            if (session.getConnectionType().matches(connectionTypes)) {
+                sessions.add(session);
             }
-
-            return sessions;
         }
+
+        return sessions;
     }
 
     public Set<String> getSessionNames() {

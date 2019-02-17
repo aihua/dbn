8dd13ccaa1784ec2b9d3d7ed9b1b3264db4a333a
dancioca <dancioca@bluewin.ch>
2019-02-17 20:49:50 +0100

    Credential manager

--- src/com/dci/intellij/dbn/credentials/DatabaseCredentialManager.java
+++ src/com/dci/intellij/dbn/credentials/DatabaseCredentialManager.java
@@ -1,16 +1,12 @@
 package com.dci.intellij.dbn.credentials;
 
-import com.dci.intellij.dbn.common.thread.SimpleLaterInvocator;
 import com.dci.intellij.dbn.connection.ConnectionId;
 import com.intellij.credentialStore.CredentialAttributes;
 import com.intellij.ide.passwordSafe.PasswordSafe;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.components.BaseComponent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.concurrent.atomic.AtomicReference;
 
 public class DatabaseCredentialManager implements BaseComponent {
 
@@ -24,24 +20,17 @@ public class DatabaseCredentialManager implements BaseComponent {
     }
 
     public void setPassword(ConnectionId connectionId, String userName, @Nullable String password) {
-        SimpleLaterInvocator.invoke(ModalityState.NON_MODAL, () -> {
-            String serviceName = getConnectionServiceName(connectionId);
-            CredentialAttributes credentialAttributes = new CredentialAttributes(serviceName, userName, null, false);
-            PasswordSafe.getInstance().setPassword(credentialAttributes, password);
-        });
+        String serviceName = getConnectionServiceName(connectionId);
+        CredentialAttributes credentialAttributes = new CredentialAttributes(serviceName, userName, this.getClass(), false);
+        PasswordSafe.getInstance().setPassword(credentialAttributes, password);
     }
 
     @Nullable
     public String getPassword(ConnectionId connectionId, String userName) {
-        AtomicReference<String> password = new AtomicReference<>();
-        SimpleLaterInvocator.invoke(ModalityState.NON_MODAL, () -> {
-            String serviceName = getConnectionServiceName(connectionId);
-            PasswordSafe passwordSafe = PasswordSafe.getInstance();
-            CredentialAttributes credentialAttributes = new CredentialAttributes(serviceName, userName, null, false);
-            String pwd = passwordSafe.getPassword(credentialAttributes);
-            password.set(pwd);
-        });
-        return password.get();
+        String serviceName = getConnectionServiceName(connectionId);
+        PasswordSafe passwordSafe = PasswordSafe.getInstance();
+        CredentialAttributes credentialAttributes = new CredentialAttributes(serviceName, userName, this.getClass(), false);
+        return passwordSafe.getPassword(credentialAttributes);
     }
 
     private boolean isMemoryStorage() {

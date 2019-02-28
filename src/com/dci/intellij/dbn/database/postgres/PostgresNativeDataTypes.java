2a06c0cb87274e269ae3f1fde6f09d192742a17a
dancioca <dancioca@bluewin.ch>
2019-02-28 21:24:36 +0100

    Postgres native data types

--- src/com/dci/intellij/dbn/database/postgres/PostgresNativeDataTypes.java
+++ src/com/dci/intellij/dbn/database/postgres/PostgresNativeDataTypes.java
@@ -4,7 +4,6 @@ import com.dci.intellij.dbn.data.type.GenericDataType;
 import com.dci.intellij.dbn.database.common.DatabaseNativeDataTypes;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.sql.Array;
 import java.sql.Date;
 import java.sql.Time;
@@ -19,12 +18,13 @@ public class PostgresNativeDataTypes extends DatabaseNativeDataTypes {
         createNumericDefinition("TID", BigDecimal.class, Types.NUMERIC);
 
         createNumericDefinition("SMALLINT", Integer.class, Types.SMALLINT);
+        createNumericDefinition("TINYINT", Integer.class, Types.TINYINT);
         createNumericDefinition("INTEGER", Long.class, Types.BIGINT);
         createNumericDefinition("INT", Integer.class, Types.INTEGER);
         createNumericDefinition("INT2", Integer.class, Types.INTEGER);
         createNumericDefinition("INT4", Integer.class, Types.INTEGER);
         createNumericDefinition("INT8", Long.class, Types.NUMERIC);
-        createNumericDefinition("BIGINT", BigInteger.class, Types.BIGINT);
+        createNumericDefinition("BIGINT", Long.class, Types.BIGINT);
         createNumericDefinition("SMALLSERIAL", Integer.class, Types.SMALLINT);
         createNumericDefinition("SERIAL", Long.class, Types.BIGINT);
         createNumericDefinition("BIGSERIAL", Long.class, Types.BIGINT);
@@ -34,13 +34,15 @@ public class PostgresNativeDataTypes extends DatabaseNativeDataTypes {
         createNumericDefinition("SERIAL8", Long.class, Types.BIGINT);
         createNumericDefinition("DECIMAL", BigDecimal.class, Types.DECIMAL);
         createNumericDefinition("NUMERIC", BigDecimal.class, Types.NUMERIC);
-        createNumericDefinition("REAL", BigDecimal.class, Types.NUMERIC);
+        createNumericDefinition("REAL", Float.class, Types.NUMERIC);
+        createNumericDefinition("DOUBLE", Float.class, Types.DOUBLE);
         createNumericDefinition("DOUBLE_PRECISION", BigDecimal.class, Types.DOUBLE);
         createNumericDefinition("FLOAT4", Float.class, Types.FLOAT);
         createNumericDefinition("FLOAT8", Float.class, Types.FLOAT);
         createNumericDefinition("MONEY", BigDecimal.class, Types.DOUBLE);
 
         createLiteralDefinition("CHARACTER VARYING", String.class, Types.VARCHAR);
+        createLiteralDefinition("LONGVARCHAR", String.class, Types.LONGNVARCHAR);
         createLiteralDefinition("VARCHAR", String.class, Types.VARCHAR);
         createLiteralDefinition("CHARACTER", String.class, Types.CHAR);
         createLiteralDefinition("CHAR", String.class, Types.CHAR);
@@ -48,17 +50,22 @@ public class PostgresNativeDataTypes extends DatabaseNativeDataTypes {
         createLiteralDefinition("TEXT", String.class, Types.VARCHAR);
         createLiteralDefinition("NAME", String.class, Types.VARCHAR);
 
+        createLiteralDefinition("BINARY", String.class, Types.BINARY);
+        createLiteralDefinition("VARBINARY", String.class, Types.BINARY);
+        createLiteralDefinition("LONGVARBINARY", String.class, Types.BINARY);
         createLiteralDefinition("BYTEA", String.class, Types.BINARY);
-        createLiteralDefinition("BIT", String.class, Types.VARCHAR);
+        createLiteralDefinition("BIT", Boolean.class, Types.BOOLEAN);
         createLiteralDefinition("BIT VARYING", String.class, Types.VARCHAR);
         createLiteralDefinition("VARBIT", String.class, Types.VARCHAR);
 
 
         createDateTimeDefinition("TIMESTAMP", Timestamp.class, Types.TIMESTAMP);
-        createDateTimeDefinition("TIMESTAMP WITH TIME ZONE", Timestamp.class, Types.TIMESTAMP);
+        createDateTimeDefinition("TIMESTAMPTZ", Timestamp.class, Types.TIMESTAMP_WITH_TIMEZONE);
+        createDateTimeDefinition("TIMESTAMP WITH TIME ZONE", Timestamp.class, Types.TIMESTAMP_WITH_TIMEZONE);
         createDateTimeDefinition("TIMESTAMP WITHOUT TIME ZONE", Timestamp.class, Types.TIMESTAMP);
         createDateTimeDefinition("DATE", Date.class, Types.DATE);
         createDateTimeDefinition("TIME", Time.class, Types.TIME);
+        createDateTimeDefinition("TIMETZ", Timestamp.class, Types.TIME_WITH_TIMEZONE);
         createDateTimeDefinition("TIME WITH TIME ZONE", Time.class, Types.TIME);
         createDateTimeDefinition("TIME WITHOUT TIME ZONE", Time.class, Types.TIME);
         createLiteralDefinition("INTERVAL", String.class, Types.VARCHAR);

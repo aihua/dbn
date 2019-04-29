3e3bb867650412d95b76b18b887b6ec6a6d0acd1
CiocaDa <dancioca@bluewin.ch>
2019-04-29 11:07:39 +0200

    db metadata layer (cache simplification - reverted)

--- src/com/dci/intellij/dbn/common/cache/Cache.java
+++ src/com/dci/intellij/dbn/common/cache/Cache.java
@@ -1,21 +1,34 @@
 package com.dci.intellij.dbn.common.cache;
 
 import com.dci.intellij.dbn.common.routine.ThrowableCallable;
-import com.intellij.util.containers.ContainerUtil;
+import gnu.trove.THashMap;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 public class Cache {
-    private Map<String, Object> elements = ContainerUtil.createSoftMap();
+    private Map<String, CacheValue> elements = new THashMap<String, CacheValue>();
+    private int expiryTimeMillis = -1;
+
+    public Cache(int expiryTimeMillis) {
+        this.expiryTimeMillis = expiryTimeMillis;
+    }
 
     @Nullable
     private <T> T get(String key) {
-        return (T) elements.get(key);
+        CacheValue<T> cacheValue = elements.get(key);
+        if (isValid(cacheValue)) {
+            return cacheValue.getValue();
+        }
+        return null;
     }
 
     private <T> void set(String key, T value) {
-        elements.put(key, value);
+        elements.put(key, new CacheValue<T>(value));
+    }
+
+    private boolean isValid(CacheValue cacheValue) {
+        return cacheValue != null && !cacheValue.isOlderThan(expiryTimeMillis);
     }
 
     public <T, E extends Throwable> T get(String key, ThrowableCallable<T, E> loader) throws E {

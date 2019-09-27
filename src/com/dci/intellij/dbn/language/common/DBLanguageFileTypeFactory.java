3bd0c73707b1ad53d20d818bc66abd2161e1e92d
Dan Cioca <dancioca@bluewin.ch>
2019-09-27 11:52:16 +0200

    File type factory deprecation

--- src/com/dci/intellij/dbn/language/common/DBLanguageFileTypeFactory.java
+++ src/com/dci/intellij/dbn/language/common/DBLanguageFileTypeFactory.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import org.jetbrains.annotations.NotNull;
 
+@Deprecated
 public abstract class DBLanguageFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer consumer) {
--- src/com/dci/intellij/dbn/language/sql/SQLFileType.java
+++ src/com/dci/intellij/dbn/language/sql/SQLFileType.java
@@ -3,23 +3,16 @@ package com.dci.intellij.dbn.language.sql;
 import com.dci.intellij.dbn.common.Icons;
 import com.dci.intellij.dbn.editor.DBContentType;
 import com.dci.intellij.dbn.language.common.DBLanguageFileType;
-import com.intellij.lang.Language;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
 public class SQLFileType extends DBLanguageFileType {
 
-    public static final SQLFileType INSTANCE = new SQLFileType(
-            SQLLanguage.INSTANCE,
-            "sql", "SQL files (DBN)", DBContentType.CODE);
+    public static final SQLFileType INSTANCE = new SQLFileType();
 
-    public static final SQLFileType DATA_FILE_TYPE = new SQLFileType(
-            SQLLanguage.INSTANCE,
-            "ds", "Dataset", DBContentType.DATA);
-
-    public SQLFileType(@NotNull Language language, String extension, String description, DBContentType contentType) {
-        super(language, extension, description, contentType);
+    public SQLFileType() {
+        super(SQLLanguage.INSTANCE, "sql", "SQL files (DBN)", DBContentType.CODE);
     }
 
 

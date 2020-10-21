891157dd2eb61870931b44772c1661976dbc3821
Dan Cioca <dancioca@bluewin.ch>
2020-10-21 20:18:18 +0200

    Compatibility issues

--- src/com/dci/intellij/dbn/connection/console/DatabaseConsoleFileInitializer.java
+++ src/com/dci/intellij/dbn/connection/console/DatabaseConsoleFileInitializer.java
@@ -6,11 +6,12 @@ import com.dci.intellij.dbn.editor.code.content.GuardedBlockMarkers;
 import com.dci.intellij.dbn.editor.code.content.GuardedBlockType;
 import com.dci.intellij.dbn.vfs.file.DBConsoleVirtualFile;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManagerAdapter;
 import com.intellij.openapi.fileEditor.FileDocumentManagerListener;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
-public class DatabaseConsoleFileInitializer implements FileDocumentManagerListener {
+public class DatabaseConsoleFileInitializer extends FileDocumentManagerAdapter implements FileDocumentManagerListener {
     @Override
     public void fileContentLoaded(@NotNull VirtualFile file, @NotNull Document document) {
         if (file instanceof DBConsoleVirtualFile) {

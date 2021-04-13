761c1ef7c35ea4c7d1b7d4fe7b08ebb5e457c265
Dan Cioca <dancioca@bluewin.ch>
2021-04-13 10:54:32 +0200

    loading state assertions

--- src/com/dci/intellij/dbn/options/DefaultProjectSettingsManager.java
+++ src/com/dci/intellij/dbn/options/DefaultProjectSettingsManager.java
@@ -3,6 +3,7 @@ package com.dci.intellij.dbn.options;
 import com.dci.intellij.dbn.DatabaseNavigator;
 import com.dci.intellij.dbn.common.component.ApplicationComponent;
 import com.dci.intellij.dbn.common.latent.Latent;
+import com.intellij.diagnostic.LoadingState;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
@@ -30,8 +31,9 @@ public class DefaultProjectSettingsManager implements ApplicationComponent, Pers
         return ApplicationManager.getApplication().getComponent(DefaultProjectSettingsManager.class);
     }
 
+    @Nullable
     public ProjectSettings getDefaultProjectSettings() {
-        return defaultProjectSettings.get();
+        return LoadingState.COMPONENTS_LOADED.isOccurred() ? defaultProjectSettings.get() : null;
     }
 
     @NotNull
@@ -46,14 +48,20 @@ public class DefaultProjectSettingsManager implements ApplicationComponent, Pers
     @Nullable
     @Override
     public Element getState() {
-        Element element = new Element("state");
-        getDefaultProjectSettings().writeConfiguration(element);
-        return element;
+        ProjectSettings projectSettings = getDefaultProjectSettings();
+        if (projectSettings != null) {
+            Element element = new Element("state");
+            projectSettings.writeConfiguration(element);
+            return element;
+        }
+        return null;
     }
 
     @Override
     public void loadState(@NotNull Element element) {
-        ApplicationManager.getApplication().invokeLater(() ->
-                getDefaultProjectSettings().readConfiguration(element));
+        ProjectSettings projectSettings = getDefaultProjectSettings();
+        if (projectSettings != null) {
+            projectSettings.readConfiguration(element);
+        }
     }
 }

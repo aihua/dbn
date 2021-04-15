5ec0bfdf05add400920c7983e62f07c97774b4dc
Dan Cioca <dancioca@bluewin.ch>
2021-04-15 13:16:07 +0200

    DBNE-2382 default proj settings state - init issue hack

--- src/com/dci/intellij/dbn/options/DefaultProjectSettingsManager.java
+++ src/com/dci/intellij/dbn/options/DefaultProjectSettingsManager.java
@@ -3,7 +3,7 @@ package com.dci.intellij.dbn.options;
 import com.dci.intellij.dbn.DatabaseNavigator;
 import com.dci.intellij.dbn.common.component.ApplicationComponent;
 import com.dci.intellij.dbn.common.latent.Latent;
-import com.intellij.ide.ApplicationInitializedListener;
+import com.intellij.diagnostic.LoadingState;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
@@ -18,19 +18,13 @@ import org.jetbrains.annotations.Nullable;
     name = "DBNavigator.DefaultProject.Settings",
     storages = @Storage(DatabaseNavigator.STORAGE_FILE)
 )
-public class DefaultProjectSettingsManager implements ApplicationComponent, PersistentStateComponent<Element>, ApplicationInitializedListener {
-    private boolean componentsInitialised = false;
+public class DefaultProjectSettingsManager implements ApplicationComponent, PersistentStateComponent<Element> {
 
-    private final Latent<ProjectSettings> defaultProjectSettings = Latent.basic(() -> {
+    private final Latent<ProjectSettings> defaultProjectSettings = Latent.basic(() ->  {
         ProjectManager projectManager = ProjectManager.getInstance();
         Project defaultProject = projectManager.getDefaultProject();
         return new ProjectSettings(defaultProject);
     });
-
-    @Override
-    public void componentsInitialized() {
-        componentsInitialised = true;
-    }
 
     private DefaultProjectSettingsManager() {}
 
@@ -38,9 +32,9 @@ public class DefaultProjectSettingsManager implements ApplicationComponent, Pers
         return ApplicationManager.getApplication().getComponent(DefaultProjectSettingsManager.class);
     }
 
-    @Nullable
+    @NotNull
     public ProjectSettings getDefaultProjectSettings() {
-        return componentsInitialised ? defaultProjectSettings.get() : null;
+        return defaultProjectSettings.get();
     }
 
     @NotNull
@@ -55,8 +49,8 @@ public class DefaultProjectSettingsManager implements ApplicationComponent, Pers
     @Nullable
     @Override
     public Element getState() {
-        ProjectSettings projectSettings = getDefaultProjectSettings();
-        if (projectSettings != null) {
+        if (LoadingState.COMPONENTS_LOADED.isOccurred()) {
+            ProjectSettings projectSettings = getDefaultProjectSettings();
             Element element = new Element("state");
             projectSettings.writeConfiguration(element);
             return element;
@@ -67,8 +61,6 @@ public class DefaultProjectSettingsManager implements ApplicationComponent, Pers
     @Override
     public void loadState(@NotNull Element element) {
         ProjectSettings projectSettings = getDefaultProjectSettings();
-        if (projectSettings != null) {
-            projectSettings.readConfiguration(element);
-        }
+        projectSettings.readConfiguration(element);
     }
 }

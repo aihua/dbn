3a16f224281e8daec91efddb2cb7ebee17b142ff
Dan Cioca <dancioca@bluewin.ch>
2022-11-07 21:18:13 +0100

    Compatibility issues

--- src/com/dci/intellij/dbn/common/component/Components.java
+++ src/com/dci/intellij/dbn/common/component/Components.java
@@ -1,6 +1,6 @@
 package com.dci.intellij.dbn.common.component;
 
-import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,11 +10,11 @@ public class Components {
 
     @NotNull
     public static <T extends ProjectComponent> T projectService(@NotNull Project project, @NotNull Class<T> interfaceClass) {
-        return project.getComponent(interfaceClass);
+        return ServiceManager.getService(project, interfaceClass);
     }
 
     @NotNull
     public static <T extends ApplicationComponent> T applicationService(@NotNull Class<T> interfaceClass) {
-        return ApplicationManager.getApplication().getComponent(interfaceClass);
+        return ServiceManager.getService(interfaceClass);
     }
 }

9eaecd1b60dcb6f4f3ce1ab482979110d88caf9e
Dan Cioca <dancioca@bluewin.ch>
2022-11-11 16:45:36 +0100

    random exceptions in the logs (CCE)

--- src/main/java/com/dci/intellij/dbn/navigation/psi/DBObjectPsiFile.java
+++ src/main/java/com/dci/intellij/dbn/navigation/psi/DBObjectPsiFile.java
@@ -19,17 +19,10 @@ import com.intellij.openapi.fileTypes.UnknownFileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.UserDataHolderBase;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.FileViewProvider;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiInvalidElementAccessException;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.ResolveState;
+import com.intellij.psi.*;
 import com.intellij.psi.scope.PsiScopeProcessor;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.PsiElementProcessor;
@@ -38,9 +31,9 @@ import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.Icon;
+import javax.swing.*;
 
-public class DBObjectPsiFile implements PsiFile, Disposable {
+public class DBObjectPsiFile extends UserDataHolderBase implements PsiFile, Disposable {
     private final DBObjectRef<?> objectRef;
 
     public DBObjectPsiFile(DBObjectRef<?> objectRef) {

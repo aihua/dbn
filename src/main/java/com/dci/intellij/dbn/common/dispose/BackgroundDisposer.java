3b0f9e9ed6e14fc9e954403f9cb6bdca9efc937d
Dan Cioca <dancioca@bluewin.ch>
2022-11-12 15:10:54 +0100

    Compatibility issues

--- src/main/java/com/dci/intellij/dbn/common/dispose/BackgroundDisposer.java
+++ src/main/java/com/dci/intellij/dbn/common/dispose/BackgroundDisposer.java
@@ -1,7 +1,7 @@
 package com.dci.intellij.dbn.common.dispose;
 
 import com.dci.intellij.dbn.common.compatibility.Compatibility;
-import com.dci.intellij.dbn.common.component.ApplicationComponent;
+import com.dci.intellij.dbn.common.event.ApplicationEvents;
 import com.dci.intellij.dbn.common.thread.Background;
 import com.dci.intellij.dbn.common.thread.ThreadMonitor;
 import com.dci.intellij.dbn.common.thread.ThreadProperty;
@@ -22,7 +22,7 @@ import static com.dci.intellij.dbn.common.thread.ThreadMonitor.isDisposerProcess
 @Slf4j
 @Getter
 @Setter
-public final class BackgroundDisposer implements ApplicationComponent {
+public final class BackgroundDisposer {
     private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
     private volatile boolean running;
     private volatile boolean exiting = false;
@@ -43,13 +43,7 @@ public final class BackgroundDisposer implements ApplicationComponent {
                 exiting = true;
             }
         });
-    }
 
-    public static class ApplicationLifecycleListener extends AppLifecycleListener.Adapter() {
-        @Override
-        public void appWillBeClosed(boolean isRestart) {
-            INSTANCE.setExiting(true);
-        }
     }
 
     public static void queue(Runnable runnable) {

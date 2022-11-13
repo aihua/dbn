bf5330efa37b643e64c32e1f3fd84dcb2e4003ae
Dan Cioca <dancioca@bluewin.ch>
2022-11-13 14:42:22 +0100

    DBNE-4575 Project Services - post project disposal issues

--- src/main/java/com/dci/intellij/dbn/common/component/Components.java
+++ src/main/java/com/dci/intellij/dbn/common/component/Components.java
@@ -1,5 +1,6 @@
 package com.dci.intellij.dbn.common.component;
 
+import com.dci.intellij.dbn.common.dispose.Failsafe;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
@@ -10,7 +11,7 @@ public class Components {
 
     @NotNull
     public static <T extends ProjectComponent> T projectService(@NotNull Project project, @NotNull Class<T> interfaceClass) {
-        return project.getService(interfaceClass);
+        return Failsafe.nd(project).getService(interfaceClass);
     }
 
     @NotNull

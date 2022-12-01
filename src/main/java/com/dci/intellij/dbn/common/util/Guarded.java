d2beadb4f25c0d6ccfa96ef12368ccdc70cd6415
Dan Cioca <dancioca@bluewin.ch>
2022-12-01 21:46:47 +0100

    DBNE-4876

--- src/main/java/com/dci/intellij/dbn/common/util/Guarded.java
+++ src/main/java/com/dci/intellij/dbn/common/util/Guarded.java
@@ -1,5 +1,6 @@
 package com.dci.intellij.dbn.common.util;
 
+import com.dci.intellij.dbn.common.dispose.AlreadyDisposedException;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import lombok.SneakyThrows;
 import org.jetbrains.annotations.Nullable;
@@ -16,12 +17,27 @@ public final class Guarded {
             return callable == null ? defaultValue : callable.call();
         } catch (ProcessCanceledException | IllegalStateException /*| UnsupportedOperationException*/ | AbstractMethodError ignore){
             return defaultValue;
+        } catch (Exception e) {
+            // DBNE-4876 (????!!)
+            if (e != AlreadyDisposedException.INSTANCE) {
+                throw e;
+            }
+            return defaultValue;
+
         }
+
     }
 
     public static void run(@Nullable Runnable runnable){
         try {
             if (runnable != null) runnable.run();
-        } catch (ProcessCanceledException | IllegalStateException /*| UnsupportedOperationException*/ | AbstractMethodError ignore){}
+        } catch (ProcessCanceledException | IllegalStateException /*| UnsupportedOperationException*/ | AbstractMethodError ignore){
+        } catch (Exception e) {
+            // DBNE-4876 (????!!)
+            if (e != AlreadyDisposedException.INSTANCE) {
+                throw e;
+            }
+        }
+
     }
 }

1a59724b503bad810478673b2ecd8766254d1fdf
Dan Cioca <dancioca@bluewin.ch>
2023-05-22 00:21:30 +0200

    Tabbed-pane disposer

--- src/main/java/com/dci/intellij/dbn/common/ui/tab/TabbedPane.java
+++ src/main/java/com/dci/intellij/dbn/common/ui/tab/TabbedPane.java
@@ -3,13 +3,19 @@ package com.dci.intellij.dbn.common.ui.tab;
 import com.dci.intellij.dbn.common.dispose.Disposer;
 import com.dci.intellij.dbn.common.dispose.StatefulDisposable;
 import com.dci.intellij.dbn.common.ui.form.DBNForm;
+import com.dci.intellij.dbn.common.util.Commons;
+import com.dci.intellij.dbn.common.util.Unsafe;
 import com.intellij.openapi.util.ActionCallback;
 import com.intellij.ui.tabs.TabInfo;
 import com.intellij.ui.tabs.impl.JBTabsImpl;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
+
+import static com.dci.intellij.dbn.common.util.Commons.nvl;
 
 public class TabbedPane extends JBTabsImpl implements StatefulDisposable {
     private boolean disposed;
@@ -85,7 +91,10 @@ public class TabbedPane extends JBTabsImpl implements StatefulDisposable {
         if (disposed) return;
 
         disposed = true;
-        for (TabInfo tabInfo : myInfo2Label.keySet()) {
+        Collection<TabInfo> tabInfos = nvl(Commons.coalesce(
+                () -> Unsafe.silent(null, () -> myInfo2Label.keySet()),
+                () -> Unsafe.silent(null, () -> getTabs())), Collections.emptyList());
+        for (TabInfo tabInfo : tabInfos) {
             Object object = tabInfo.getObject();
             tabInfo.setObject(null);
             Disposer.dispose(object);

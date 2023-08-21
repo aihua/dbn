facb3883dc3fea36223dd80d0d393b2a5ed3db26
dancioca <dancioca@bluewin.ch>
2023-08-21 22:03:48 +0200

    Console and Code Editors - double gutter actions

--- src/main/java/com/dci/intellij/dbn/editor/code/SourceCodeEditorProviderBase.java
+++ src/main/java/com/dci/intellij/dbn/editor/code/SourceCodeEditorProviderBase.java
@@ -1,7 +1,6 @@
 package com.dci.intellij.dbn.editor.code;
 
 import com.dci.intellij.dbn.common.dispose.Disposer;
-import com.dci.intellij.dbn.common.dispose.Failsafe;
 import com.dci.intellij.dbn.common.editor.BasicTextEditorProvider;
 import com.dci.intellij.dbn.common.environment.EnvironmentManager;
 import com.dci.intellij.dbn.common.exception.ProcessDeferredException;
@@ -17,11 +16,12 @@ import com.intellij.ide.impl.StructureViewWrapperImpl;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorImpl;
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
@@ -44,50 +44,62 @@ abstract class SourceCodeEditorProviderBase extends BasicTextEditorProvider impl
         DBEditableObjectVirtualFile databaseFile;
         boolean temporary = false;
 
-        EditorProviderId editorProviderId = getEditorProviderId();
         if (file instanceof DBSourceCodeVirtualFile) {
             DBSourceCodeVirtualFile sourceCodeFile = (DBSourceCodeVirtualFile) file;
             databaseFile = sourceCodeFile.getMainDatabaseFile();
 
             temporary = Traces.isCalledThrough(StructureViewWrapperImpl.class);
-            if (!temporary) {
-                // trigger main file editor and cancel the original request
-                // (prevent creation of editor if invoked for the source code file -  e.g. debugger navigation / file bookmarks)
-                openMainFileEditor(databaseFile);
-            }
+            if (temporary) return createBasicEditor(project, sourceCodeFile);
+
+            // trigger main file editor and cancel the original request
+            // (prevent creation of editor if invoked for the source code file -  e.g. debugger navigation / file bookmarks)
+            openMainFileEditor(databaseFile);
         } else {
             databaseFile = (DBEditableObjectVirtualFile) file;
         }
 
-        DBSourceCodeVirtualFile sourceCodeFile = Failsafe.nn(getSourceCodeFile(databaseFile));
-        boolean isMainEditor = sourceCodeFile.getContentType() == databaseFile.getMainContentType();
+        DBSourceCodeVirtualFile sourceCodeFile = databaseFile.ensureContentFile(getContentType());
+        SourceCodeEditor sourceCodeEditor = createCodeEditor(project, sourceCodeFile);
+        return prepareEditor(project, sourceCodeEditor, sourceCodeFile);
+    }
 
+    @NotNull
+    private static PsiAwareTextEditorImpl createBasicEditor(@NotNull Project project, DBSourceCodeVirtualFile sourceCodeFile) {
+        return new PsiAwareTextEditorImpl(project, sourceCodeFile, TextEditorProvider.getInstance());
+    }
+
+    @NotNull
+    private SourceCodeEditor createCodeEditor(@NotNull Project project, DBSourceCodeVirtualFile sourceCodeFile) {
+        DBEditableObjectVirtualFile databaseFile = sourceCodeFile.getMainDatabaseFile();
         String editorName = getName();
+        boolean isMainEditor = sourceCodeFile.getContentType() == databaseFile.getMainContentType();
+        EditorProviderId editorProviderId = getEditorProviderId();
         SourceCodeEditor sourceCodeEditor = isMainEditor ?
                 new SourceCodeMainEditor(project, sourceCodeFile, editorName, editorProviderId) :
                 new SourceCodeEditor(project, sourceCodeFile, editorName, editorProviderId);
+        return sourceCodeEditor;
+    }
 
-        if (!temporary) {
-            Editor editor = sourceCodeEditor.getEditor();
-            Document document = editor.getDocument();
-
-            EnvironmentManager environmentManager = EnvironmentManager.getInstance(project);
-            if (environmentManager.isReadonly(sourceCodeFile) || !sourceCodeFile.isLoaded()) {
-                Editors.setEditorReadonly(editor, true);
-            }
-
-            int documentSignature = document.hashCode();
-            if (document.hashCode() != sourceCodeFile.getDocumentSignature()) {
-                document.addDocumentListener(sourceCodeFile);
-                sourceCodeFile.setDocumentSignature(documentSignature);
-            }
-
-            Icon icon = getIcon();
-            if (icon != null) {
-                updateTabIcon(databaseFile, sourceCodeEditor, icon);
-            }
+    private SourceCodeEditor prepareEditor(@NotNull Project project, SourceCodeEditor sourceCodeEditor, DBSourceCodeVirtualFile sourceCodeFile) {
+        Editor editor = sourceCodeEditor.getEditor();
+        EnvironmentManager environmentManager = EnvironmentManager.getInstance(project);
+        if (environmentManager.isReadonly(sourceCodeFile) || !sourceCodeFile.isLoaded()) {
+            Editors.setEditorReadonly(editor, true);
         }
 
+
+        Document document = editor.getDocument();
+        int documentSignature = document.hashCode();
+        if (document.hashCode() != sourceCodeFile.getDocumentSignature()) {
+            document.addDocumentListener(sourceCodeFile);
+            sourceCodeFile.setDocumentSignature(documentSignature);
+        }
+
+        Icon icon = getIcon();
+        if (icon != null) {
+            DBEditableObjectVirtualFile databaseFile = sourceCodeFile.getMainDatabaseFile();
+            updateTabIcon(databaseFile, sourceCodeEditor, icon);
+        }
         return sourceCodeEditor;
     }
 
@@ -106,11 +118,6 @@ abstract class SourceCodeEditorProviderBase extends BasicTextEditorProvider impl
             return objectVirtualFile.getContentFile(getContentType());
         }
         return super.getContentVirtualFile(virtualFile);
-    }
-
-    @Nullable
-    private DBSourceCodeVirtualFile getSourceCodeFile(DBEditableObjectVirtualFile databaseFile) {
-        return databaseFile.getContentFile(getContentType());
     }
 
     public abstract DBContentType getContentType();

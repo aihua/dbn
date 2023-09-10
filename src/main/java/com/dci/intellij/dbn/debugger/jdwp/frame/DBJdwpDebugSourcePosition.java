9da35541c8ad02758ddf12268dd2b745629e3371
Dan Cioca <dancioca@bluewin.ch>
2023-09-10 17:54:47 +0200

    DBN-582 Jdwp Debugger - custom source position handling (alternative to ProcessDeferredException)

---
+++ src/main/java/com/dci/intellij/dbn/debugger/jdwp/frame/DBJdwpDebugSourcePosition.java
@@ -0,0 +1,46 @@
+package com.dci.intellij.dbn.debugger.jdwp.frame;
+
+import com.dci.intellij.dbn.vfs.file.DBSourceCodeVirtualFile;
+import com.intellij.debugger.SourcePosition;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class DBJdwpDebugSourcePosition extends SourcePosition {
+    private final PsiFile file;
+    private final int line;
+
+    public DBJdwpDebugSourcePosition(PsiFile file, int line) {
+        this.file = file;
+        this.line = line;
+    }
+
+    @NotNull
+    public PsiFile getFile() {
+        VirtualFile virtualFile = file.getVirtualFile();
+        if (virtualFile instanceof DBSourceCodeVirtualFile) {
+            DBSourceCodeVirtualFile sourceCodeVirtualFile = (DBSourceCodeVirtualFile) virtualFile;
+            return sourceCodeVirtualFile.getObject().getPsiCache().getPsiFile();
+        }
+        return file;
+    }
+
+    @Override
+    public PsiElement getElementAt() {
+        return null;
+    }
+
+    @Override
+    public int getOffset() {
+        return 0;
+    }
+
+    @Override
+    public Editor openEditor(boolean requestFocus) {
+        return null;
+    }
+}
